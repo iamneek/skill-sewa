3e430baa9aafa44ad04822202c278bc74244a86b
Coolbit <neeoub@gmail.com>
2026-05-01 19:12:15 +0545

    ADDED: DBConnection file written, and tested - WORKS FINE

--- src/main/java/com/inception/skillsewa/utils/DBConnection.java
+++ src/main/java/com/inception/skillsewa/utils/DBConnection.java
@@ -1,5 +1,35 @@
 package com.inception.skillsewa.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/skillsewa";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+//    public static void main(String[] args) throws SQLException{
+//        try{
+//            Connection conn = getConnection();
+//            if (conn != null){
+//                System.out.println("Connection to database success.");
+//            }
+//            else {
+//                System.out.println("Connection to database fail.");
+//            }
+//        } catch (SQLException e){
+//            throw new SQLException("JDBC Driver Problem!!");
+//        }
+//    }
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("JDBC Driver Problem!!");
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 
 }
